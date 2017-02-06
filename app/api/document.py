3456027b64d049a9e5e44cf024160c6be4d2300a
kentsay <naturen@gmail.com>
2017-02-06 13:11:41 +0100

    Modify write request(POST, DEL) into asynchronous mode

--- app/api/document.py
+++ app/api/document.py
@@ -3,17 +3,35 @@
 import falcon
 import string
 import json
+from rq import Queue
+from rq.job import Job
+from writer import postDocument, delDocument
 
-documents_dict = {}
-documents_index = []
+"""
+Todo list:
+3. modify redis indexing for query value and get key
+4. redix better way to handle set & get (mset, etc)
+5. api error handling
+"""
 
 class DocumentResource(object):
 
     def __init__(self, db):
         self.db = db
 
+    # This function handles GET reuqests
+    def on_get(self, req, resp, doc_index):
+        try:
+            r = self.db.connection()
+        except Exception as ex:
+            print ex
+
+        result = r.get(doc_index)
+        resp.body = json.dumps(result)
+        resp.status = falcon.HTTP_200
+
+    # This function handles POST reuqests
     def on_post(self, req, resp, doc_index):
-        """Handles POST request"""
         try:
             raw_json = req.stream.read()
         except Exception as ex:
@@ -27,46 +45,28 @@ class DocumentResource(object):
                 'Malformed JSON',
                 'Could not decode the request body. The JSON was incorrect.')
 
-        try:
-            r = self.db.connection()
-        except Exception as ex:
-            print ex
-
-        key = doc_index
-        r.set(key, result_json)
-        documents_index.append(key)
-
-        key_word = result_json['message'].split(" ")
-        for w in key_word:
-            # strip punctuation from string and convert into lower case
-            # otherwise 'Do' and 'do' will be 2 different keywords
-            w = w.strip(string.punctuation).lower()
-            if documents_dict.has_key(w):
-                documents_dict[w].append(key)
-            else:
-                documents_dict[w] = [];
-                documents_dict[w].append(key)
+        """
+        Enqueueing write request as jobs into document_write queue
+        and processing them in the background with workers.
+        """
+        q = Queue('document_write', connection=self.db.connection())
+        job = q.enqueue_call(
+            func=postDocument, args=(result_json, doc_index), result_ttl=5000
+        )
+        print(job.get_id())
 
         resp.status = falcon.HTTP_202
         resp.body = json.dumps(result_json, encoding='utf-8')
 
-    def on_get(self, req, resp, doc_index):
-        """Handles GET requests"""
-        try:
-            r = self.db.connection()
-        except Exception as ex:
-            print ex
 
-        result = r.get(doc_index)
-        resp.body = json.dumps(result)
-        resp.status = falcon.HTTP_200
-
-
+    # This function handles DELETE reuqests
     def on_delete(self, req, resp, doc_index):
-        """Handles DEL requests"""
-        try:
-            r = self.db.connection()
-        except Exception as ex:
-            print ex
-
-        r.delete(doc_index)
+        """
+        Enqueueing write request as jobs into document_delete queue
+        and processing them in the background with workers.
+        """
+        q = Queue('document_delete', connection=self.db.connection())
+        job = q.enqueue_call(
+            func=delDocument, args=(doc_index,), result_ttl=5000
+        )
+        print(job.get_id())
