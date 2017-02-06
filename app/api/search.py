e99e4a82d0dac4f66c988e4facad3bc75bbd36d8
kentsay <naturen@gmail.com>
2017-02-06 14:13:36 +0100

    Change memory index into redis index

--- app/api/search.py
+++ app/api/search.py
@@ -2,28 +2,40 @@
 
 import falcon
 import string
-import document
 import json
+from . import redis_db
+from app.util.stringUtil import preProcess
 
 class SearchResource(object):
 
     def __init__(self, db):
         self.db = db
 
+    # This function handles GET reuqests
     def on_get(self, req, resp):
-        """Handles GET requests"""
-        intersect = []
+        try:
+            db = redis_db.RedisStorageEngine()
+            r = db.connection()
+        except Exception as ex:
+            print ex
+
         if req.get_param("q"):
             keywords = req.get_param("q").split(" ")
             counter = 1
+            result = set()
             for keyword in keywords:
-                if document.documents_dict.has_key(keyword):
-                    # Returns the list of document IDs that match all the keywords
-                    if counter == 1:
-                        intersect = set(document.documents_dict[keyword]).intersection(set(document.documents_index))
-                    else:
-                        intersect = set(document.documents_dict[keyword]).intersection(set(intersect))
-                        counter += 1
+                """
+                Strip punctuation from string and convert into lower case.
+                otherwise 'Do' and 'do' will be 2 different keywords.
+                """
+                keyword = preProcess(keyword)
+                if counter == 1:
+                    """SMEMBERS returns all the members of the set value stored at key."""
+                    result = r.smembers(keyword)
+                else:
+                    """use set intersection to return the list of document IDs that match all the keywords"""
+                    result = result.intersection(r.smembers(keyword))
+                counter += 1
 
-            resp.body = json.dumps(list(intersect))
+            resp.body = json.dumps(list(result))
             resp.status = falcon.HTTP_200  # This is the default status
