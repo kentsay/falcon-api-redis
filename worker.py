531f249f8b0fd7d8707adeb8a04651c9494ec35e
kentsay <naturen@gmail.com>
2017-02-06 12:52:48 +0100

    add asynchronously rq workers to handle write request in background

---
+++ worker.py
@@ -0,0 +1,14 @@
+import os
+import redis
+from rq import Worker, Queue, Connection
+
+"""RQ workers listen to document_write and document_delete queue."""
+listen = ['document_write', 'document_delete']
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(list(map(Queue, listen)))
+        worker.work()
